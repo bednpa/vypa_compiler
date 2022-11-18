ea45ec866a0b60f2ac545bf206e63b27afbd60c4
Pavel Bednář <pavelbednar@dhcph240.fit.vutbr.cz>
2022-11-18 15:32:30 +0100

    parser pro expr gramatiku

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("test")
+import sys
+from antlr4 import *
+from ExprLexer import ExprLexer
+from ExprParser import ExprParser
+ 
+def main(argv):
+    input_stream = FileStream(argv[1])
+    lexer = ExprLexer(input_stream)
+    stream = CommonTokenStream(lexer)
+    parser = ExprParser(stream)
+    tree = parser.prog()
+ 
+if __name__ == '__main__':
+    main(sys.argv)
