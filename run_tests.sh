ff5d7f25043c396e8da9a40b16d0f04e45945824
Pavel Bednář <pavelbednar@Pavel-MacBook-Pro.local>
2022-11-26 18:55:15 +0100

    added grammar and tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-echo "********************************"
+echo "Running available tests ..."
+echo "*********************************"
 for filename in ./tests/*.in; do
     echo "test name:" $filename
+    line=$(head -n 1 $filename)
+    echo "description: ${line:2}"
+    echo "- - - - - - - - - - - - - - - - -"
     python3.8 ./src/main.py $filename
-    echo "********************************"
+    echo "*********************************"
 done
