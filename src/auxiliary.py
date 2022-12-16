35939330fd4db4f82c7630afdf20816d8feb263c
Pavel Bednář <pavelbednar@dhcpf007.fit.vutbr.cz>
2022-12-16 17:58:52 +0100

    updated expression checker

--- src/auxiliary.py
+++ src/auxiliary.py
@@ -6,7 +6,7 @@
 #
 # Class for evaluation of expressions.
 #
-class exprEval():
+class exprChecker():
     
     
     #
@@ -17,45 +17,45 @@ class exprEval():
         
     
     #
-    # Pushs smybol on expression stack.
+    # Pushs type on stack.
     #  
-    def push(self, elem, type):
-        self.stack.append((elem, type))
-
-
-    #
-    # Semantic check and evaluation of expressions.
-    #
-    def eval(self):
-        if len(self.stack) == 1:
-            evaluated = self.stack
-            self.stack = []
-            return evaluated[0][0], evaluated[0][1]
+    def addType(self, elem):
+        self.stack.append(elem)
         
-        while len(self.stack) > 1:
-            for i in range(len(self.stack)):
-                if self.stack[i][1] == None:
-                    if self.stack[i][0] == "+":
-                        if self.stack[i-2][1] == "int" and self.stack[i-1][1] == "int":
-                            result = self.stack[i-2][0] + self.stack[i-1][0]
-                            self.stack[i-2] = (result, "int")
-                            del self.stack[i-1]
-                            del self.stack[i-1]
-                            break
-                        else:
-                            raise typeError("+", self.stack[i-2][0], self.stack[i-1][0], self.stack[i-2][1], self.stack[i-1][1])
-                    if self.stack[i][0] == "*":
-                        if self.stack[i-2][1] == "int" and self.stack[i-1][1] == "int":
-                            result = self.stack[i-2][0] * self.stack[i-1][0]
-                            self.stack[i-2] = (result, "int")
-                            del self.stack[i-1]
-                            del self.stack[i-1]
-                            break
-                        else:
-                            raise typeError("*", self.stack[i-2][0], self.stack[i-1][0], self.stack[i-2][1], self.stack[i-1][1])
-        evaluated = self.stack
-        self.stack = []
-        return evaluated[0][0], evaluated[0][1]
+        
+    #
+    # Adds return type of two top elements.
+    #
+    def addOp(self, op):
+        o2 = self.stack.pop()
+        o1 = self.stack.pop()
+        
+        if (op == "+"):
+            if (o1 == "int" and o2 == "int"):
+                self.stack.append("int")
+            else:
+                typeError(op, o1, o2)
+                
+        elif (op == "*"):
+            if (o1 == "int" and o2 == "int"):
+                self.stack.append("int")
+            else:
+                typeError(op, o1, o2)
+                
+        else:
+            raise unexpectedError()
+                
+    
+    #
+    # Returns type of whole expression.
+    #    
+    def returnType(self):
+        type = self.stack
+        if len(self.stack) == 1:
+            self.stack = []
+            return type[0]
+        else:
+            return unexpectedError()
 
 
 #
@@ -88,8 +88,25 @@ class notDeclared(customException):
 # Class for various type errors.
 #
 class typeError(customException):
-    def __init__(self, op, val1, val2, type1, type2):
-        self.what = "type error in " + str(val1) + " " + str(op) + " " + str(val2) + " (" + str(type1) + " " + str(op) + " " + str(type2) + " is not allowed)."
-        self.err_code = 3
+    def __init__(self, op, type1, type2):
+        self.what = "type error, " + str(type1) + " " + str(op) + " " + str(type2) + " is not allowed."
+        self.err_code = 3        
         
+
+#
+# Class for inner compiler errors.
+#
+class unexpectedError(customException):
+    def __init__(self):
+        self.what = "unexpected error happens."
+        self.err_code = 4
+        
+        
+#
+# Class for bad expression in if header.
+#
+class ifHeaderError(customException):
+    def __init__(self, type):
+        self.what = "Type " + str(type) + " can not be in if header."
+        self.err_code = 5
         
