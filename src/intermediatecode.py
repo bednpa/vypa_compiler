9226f4f7e4c4fd1bd3634129b8fd81e4c2802aed
sgajdos <sgajdos@redhat.com>
2022-12-16 23:38:35 +0100

    Intermediate changes

--- src/intermediatecode.py
+++ src/intermediatecode.py
@@ -10,6 +10,8 @@ DIV, o1, o1, o2         - Divide o1 by o2 and store the result into o1
 ADD, o1, o1, o2         - Add o1 and o2 and store the result into o1
 SUB, o1, o1, o2         - Subtract o2 from o1 and store the result into o1
 
+SET o1, o2              - Set variable o1 to value o2
+
 JUMP, o1                - Unconditional jump to label o1
 JUMPIFEQ, o1, o2, o3    - Jump to label o1 if o2 == o3
 JUMPIFNEQ, o1, o2, o3   - Jump to label o1 if o2 != o3
@@ -43,10 +45,32 @@ PUSH o1
 
 '''
 
+def getInterFunctionDefinition(label_name):
+    inter_code = '''
+    LABEL {}
+        # Push FP and increment SP
+        PUSHI $FP
+        ADDI $SP, $SP, 1
+    '''.format(label_name)
+
+    return inter_code
+
+def getInterDeclaration(id_name, value):
+    inter_code = '''
+    SET {} {}'''
+
+
+
+
 
 class interCode:
     def __init__(self):
         self.code = []
+        
+        self.address_dict = {}
+        self.frame_pointer = 0
+        self.stack_pointer = 0
+
         
         
     def addCode(self, op, o1=None, o2=None, o3=None):
@@ -58,6 +82,17 @@ class interCode:
         self.addCode(self, "POP", "$1")
         self.addCode(self, op, "$1", "$1", "$2")
         self.addCode(self, "PUSHI", "$1")
+
+    def addFunctionDefinitionCode(self, o1):
+        self.addCode(self, "LABEL", o1)
+        self.addCode(self, "PUSHI", "$FP")
+        self.addCode(self, "ADD", "$SP", "$SP", 1)
+
+    def addVarDeclaration(self, o1, o2):
+        self.addCode(self, "SET", "o1", "o2")
+
+
+    
 
     
     def getAddress():
@@ -71,12 +106,36 @@ class interCode:
         '''
         Translate intermediate code to VypCode.
         '''
-        # for row in self.code:
-        #     if row["o1"] != None:
-        #         row["o1"] = self.getAddr(row["o1"])
+        for row in self.code:
 
-        #     if row["op"] == "PUSHI":
+            if row["op"] == "SET":
+                if row["o1"] in self.address_dict:
+                    row["o1"] = self.getAddress(row["o1"])
 
-        #         SET [$BP + 1], 6
-        #         ADDI $SP, $SP, 1
-        #         fprintf("SET [$SP] %s\n", row["o1"])
+                    if row["o2"][0] == "s":
+                        #TODO: Generate set for string in target code
+                        pass
+                    if row["o2"][0] == "i":
+                        #TODO: Generate set for integer in target code
+                        pass
+                    if row["o2"][0] == "v":
+                        #TODO: Generate set for variable in target code
+                        # Get value from address using []
+                        pass
+                else:
+                    #Create a new variable - push it to the stack
+                    self.setAddress(row["op"], self.stack_pointer)
+         
+                    if row["o2"][0] == "s":
+                        #TODO: Generate push for string in target code
+                        pass
+                    if row["o2"][0] == "i":
+                        #TODO: Generate push for integer in target code
+                        pass
+                    if row["o2"][0] == "v":
+                        #TODO: Generate push for variable in target code
+                        # Get value from address using []
+                        pass
+
+
+    
